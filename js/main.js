7f2570a2b8b55b2bfbeb4324d658f2e3c7b59615
Ibrokhim <machacker354@gmail.com>
2019-07-15 10:39:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+	// var section = $("section");
+
+	// $(window).scroll(function() {
+
+	// 	var T0 = section.eq(0).offset().top;
+	// 	var T1 = section.eq(1).offset().top;
+	// 	var T2 = section.eq(2).offset().top;
+	// 	var T3 = section.eq(3).offset().top;
+
+	// 	console.log(T0, T1, T2, T3);
+
+	// 	if (window.scrollY > 0) {
+	// 		$("html, body").animate({
+	// 			scrollTop: T1
+	// 		});
+	// 		break;
+	// 	} else if (window.scrollY < T1) {
+	// 		$("html, body").animate({
+	// 			scrollTop: 0
+	// 		});
+	// 		break;
+	// 	}
+	// });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
